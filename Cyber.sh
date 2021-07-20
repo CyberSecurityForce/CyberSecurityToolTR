faa30f4d145c18191430ec5c48f420871f3831bf
CyberSecurityForce <87718755+CyberSecurityForce@users.noreply.github.com>
2021-07-20 21:22:11 +0300

    Add files via upload

---
+++ Cyber.sh
@@ -0,0 +1,6 @@
+echo -e "\e[32mHerhangi bir Toolu kurabilirsiniz
+
+Kurmak istediğiniz toolu kopyala yapıştır yapınız \e[0m";
+echo -e "\e[31m1. git clone     https://github.com/termuxxtoolss/SMS-ATTACK   : SMS BOMBER
+
+2. git clone https://github.com/termuxxtoolss/CAMERA-HACK   : CAMERA-HACK \e[0m";

6f790dd8c952883267acbee9fbb6d3975ba99628
CyberSecurityForce <87718755+CyberSecurityForce@users.noreply.github.com>
2021-07-22 17:47:52 +0300

    Add files via upload

---
+++ cyber.sh
@@ -0,0 +1,7 @@
+echo -e "\e[35mCyber Security Tool TR Hoşgeldiniz \e[0m";
+echo -e "\e[32mİstediğiniz toolu kopyala yapıştır yapınız \e[0m";
+echo -e "Sorumluluk Kullanıcı'ya aittir ";
+echo -e "\e[34mLütfen sadece eğitim Amaçlı kullanınız\e[0m";
+echo -e "\e[33mTermux-Styling : git clone https://github.com/termuxxtoolss/TERMUX-STYLING\e[0m";
+echo -e "\e[33mŞifre Oluştur : git clone https://github.com/Raven1965/SIFRE-OLUSTURUCU\e[0m";
+echo -e "\e[32mBaşka Toollar eklenecektir sürüm 1.0\e[0m";
